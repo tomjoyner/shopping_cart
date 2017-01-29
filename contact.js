c4514f3579d612041b85e4cbad3c668d7f8921c4
imnotspock <imnotspock@users.noreply.github.com>
2017-01-30 10:10:05 +1100

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,16 @@
+$('form#contact_form').validate({
+  messages: { },
+  submitHandler: function(form) {
+    $.ajax({
+      url: "//formspree.io/hello@reecreate.com",
+      method: "POST",
+      data: $(form).serialize(),
+      dataType: "json",
+      success: function(data) {
+          $("form#contact_form :input").prop("disabled", true);
+          $('#thanks').show();
+      }
+    });
+    return false;
+  }
+});
